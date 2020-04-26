7d2755ede4e6499ce652835b07e208d9e39d0c3d
Fernando Andre Alarcón Delgado <a20070808@pucp.pe>
2020-04-26 13:11:06 -0500

    Vers_A3

--- cap9/fig9_2.py
+++ cap9/fig9_2.py
@@ -36,7 +36,6 @@ x4 = np.multiply(x2,x3)
 X = np.array([x1, x2, x3, x4]).T
 Y = dict_dat['capac_pulm']
 
-
 # Se construyen los graficos
 ed_min = min(x3)
 ed_max = max(x3)
@@ -47,8 +46,8 @@ fig, axs = plt.subplots(2, 2, sharex=True, sharey=True)
 # Beta3=0; Beta4=0
 model1 = [True, True, False, False]
 Beta1 = mco_func(X[:,model1], Y)
-y_1_1 = np.multiply((Beta1[0]*1 + Beta1[1] * 1), ([1]*len(domi)))
-y_1_2 = np.multiply((Beta1[0]*1), ([1]*len(domi)))
+y_1_1 = np.multiply((Beta1[0]*1), ([1]*len(domi)))
+y_1_2 = np.multiply((Beta1[0]*1 + Beta1[1] * 1), ([1]*len(domi)))
 sns.scatterplot(x="edad", y="capac_pulm", hue="tip_ejer", data=oxi_df, ax=axs[0][0])
 axs[0,0].plot(domi, y_1_1)
 axs[0,0].plot(domi, y_1_2)
@@ -58,8 +57,8 @@ axs[0][0].legend_.remove()
 # Beta2=0; Beta4=0
 model2 = [True, False, True, False]
 Beta2 = mco_func(X[:,model2], Y)
-y_2_1 = np.add(np.multiply((Beta2[0]*1), ([1]*len(domi))), np.multiply(Beta2[1], domi))
-y_2_2 = np.add(np.multiply((Beta2[0]*1), ([1]*len(domi))), np.multiply(Beta2[1], domi)) 
+y_2_1 = np.add(np.multiply((Beta2[0]*1), ([1]*len(domi))), np.multiply(Beta2[1], domi)) 
+y_2_2 = np.add(np.multiply((Beta2[0]*1), ([1]*len(domi))), np.multiply(Beta2[1], domi))
 sns.scatterplot(x="edad", y="capac_pulm", hue="tip_ejer", data=oxi_df, ax=axs[0][1])
 axs[0,1].plot(domi, y_2_1)
 axs[0,1].plot(domi, y_2_2)
@@ -69,8 +68,8 @@ axs[0][1].legend_.remove()
 # Beta4=0
 model3 = [True, True, True, False]
 Beta3 = mco_func(X[:,model3], Y)
-y_3_1 = np.add(np.multiply((Beta3[0]*1 + Beta3[1]*1), ([1]*len(domi))), np.multiply(Beta3[2], domi))
-y_3_2 = np.add(np.multiply((Beta3[0]*1), ([1]*len(domi))), np.multiply(Beta3[2], domi)) 
+y_3_1 = np.add(np.multiply((Beta3[0]*1), ([1]*len(domi))), np.multiply(Beta3[2], domi)) 
+y_3_2 = np.add(np.multiply((Beta3[0]*1 + Beta3[1]*1), ([1]*len(domi))), np.multiply(Beta3[2], domi))
 sns.scatterplot(x="edad", y="capac_pulm", hue="tip_ejer", data=oxi_df, ax=axs[1][0])
 axs[1,0].plot(domi, y_3_1)
 axs[1,0].plot(domi, y_3_2)
@@ -80,8 +79,8 @@ axs[1][0].legend_.remove()
 # Todas las variables
 model4 = [True, True, True, True]
 Beta4 = mco_func(X[:,model4], Y)
-y_4_1 = np.add(np.add(np.multiply((Beta4[0]*1 + Beta4[1]*1), ([1]*len(domi))), np.multiply(Beta4[2], domi)) , np.multiply(Beta4[3], domi))
-y_4_2 = np.add(np.multiply((Beta4[0]*1), ([1]*len(domi))), np.multiply(Beta4[2], domi)) 
+y_4_1 = np.add(np.multiply((Beta4[0]*1), ([1]*len(domi))), np.multiply(Beta4[2], domi)) 
+y_4_2 = np.add(np.add(np.multiply((Beta4[0]*1 + Beta4[1]*1), ([1]*len(domi))), np.multiply(Beta4[2], domi)) , np.multiply(Beta4[3], domi))
 sns.scatterplot(x="edad", y="capac_pulm", hue="tip_ejer", data=oxi_df, ax=axs[1][1])
 axs[1,1].plot(domi, y_4_1)
 axs[1,1].plot(domi, y_4_2)
